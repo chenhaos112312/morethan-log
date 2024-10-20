a1320b68a0506e0e66f31acc37bdb9ce2e490e6a
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2024-10-20 12:22:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+  output: process.env.EXPORT ? 'export' : undefined
 }
+module.exports = withBundleAnalyzer(nextConfig)
