57d7f5ca7d9582a276f42ded670f44139cab05de
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2024-10-22 17:16:06 +0800

    Update Scripts.tsx

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -18,6 +18,22 @@ const Scripts: React.FC = () => (
         </Script>
       </>
     )}
+
+    {CONFIG?.crisp?.enable === true && (
+      <>
+        <Script>
+        {`window.$crisp = [];
+        window.CRISP_WEBSITE_ID = "6c078f6c-7060-473b-ab40-639c0817c1cb";
+        (function() {
+           d = document;
+          s = d.createElement("script");
+          s.src = "https://client.crisp.chat/l.js";
+          s.async = true;
+          d.getElementsByTagName("head")[0].appendChild(s);
+        })();`}
+        </Script>
+      </>
+    )}
   </>
 )
 
